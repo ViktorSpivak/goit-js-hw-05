c2da9a2bad411b6bd715f7a787441958156e3e92
Viktor Spivak <spivakmailbox@gmail.com>
2019-09-24 21:25:55 +0300

    first commit

---
+++ task-04.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+  constructor(string) {
+    this._value = string;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    this._value = this._value.concat(str);
+  }
+  prepend(str) {
+    this._value = str.concat(this._value);
+  }
+  pad(str) {
+    this._value = this._value.concat(str);
+    this._value = str.concat(this._value);
+  }
+}
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
